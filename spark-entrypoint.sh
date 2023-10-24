889efe6b8324941cfd6b2d7a3a3ecdd1527d6e6b
wopoadi <kodjo@osekoo.com>
2023-10-24 21:07:20 +0200

    Update spark-entrypoint.sh

--- spark-entrypoint.sh
+++ spark-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "${SPARK_MODE}" == "master" ]; then
-  ${SPARK_HOME}/sbin/start-master.sh
+  /opt/spark/sbin/start-master.sh
 
 elif [ "${SPARK_MODE}" == "worker" ]; then
-  ${SPARK_HOME}/sbin/start-worker.sh "$SPARK_MASTER_URL" && wait
+  /opt/spark/sbin/start-worker.sh "$SPARK_MASTER_URL" && wait
 
 elif [ "${SPARK_MODE}" == "client" ]; then
   cd /app || exit
@@ -22,7 +22,7 @@ elif [ "${SPARK_MODE}" == "client" ]; then
     APP_DEPLOY_MODE=client
   fi
 
-  ${SPARK_HOME}/bin/spark-submit \
+  /opt/spark/bin/spark-submit \
                       --deploy-mode "$APP_DEPLOY_MODE" \
                       --master "$SPARK_MASTER_URL" \
                       --executor-cores "$SPARK_WORKER_CORES" \
